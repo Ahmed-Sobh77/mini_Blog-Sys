98065bdc96f52f4147e9e58125914fe5f4d8f767
Ahmed Sobh <sobhahmed747@gmail.com>
2026-04-23 16:52:38 +0200

    second

--- Program.cs
+++ Program.cs
@@ -6,14 +6,7 @@ namespace blog
     {
         static void Main(string[] args)
         {
-            var context = new Data.DataContext();
-            var PostService = new Services.PostService(context);
-            var UserService = new Services.UserService(context);
-            var user1=UserService.CreateUser(1, "John Doe", "john@example.com", "password123");
-            Post P= PostService.CreatePost("Hello World",user1);
-            var notifi = new Services.NotificationService();
-
-            PostService.PostCreated += notifi.OnPostCreated;
+            // still want to write flow of the system
 
         }   
     }
--- obj/Debug/net10.0/blog.AssemblyInfo.cs
+++ obj/Debug/net10.0/blog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11de366dcd768ef0f7ddc9d45f956efb481dca01")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
